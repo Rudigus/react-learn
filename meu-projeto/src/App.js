f1f4ab217225fd4b45cdbf3d7de695f3bbb207c4
Rudigus <rodrigo.matos9@hotmail.com>
2023-11-03 14:41:15 -0300

    starting my react journey

---
+++ meu-projeto/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Testando 1 2 3</h1>
+      <p>Aparentemente tá funcionando</p>
+    </div>
+  );
+}
+
+export default App;
