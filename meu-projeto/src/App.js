131cb950f3083845fc9cdc9f5a2ebb4a042f4dd0
Rudigus <rodrigo.matos9@hotmail.com>
2023-11-03 15:21:21 -0300

    basic jsx

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
 
 function App() {
+
+  const n1 = 1;
+  const n2 = 2;
+
+  function sum(a, b) {
+    return a + b;
+  }
+
   return (
     <div className="App">
       <h1>Testando 1 2 3</h1>
       <p>Aparentemente tá funcionando</p>
+      <p>A soma de {n1} com {n2} é {sum(n1, n2)}</p>
     </div>
   );
 }
