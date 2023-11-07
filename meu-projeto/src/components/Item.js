3b6b10845428a2a8eed182db73cffa63db0875c5
Rudigus <rodrigo.matos9@hotmail.com>
2023-11-07 06:43:17 -0300

    prop types

--- meu-projeto/src/components/Item.js
+++ meu-projeto/src/components/Item.js
@@ -1,9 +1,21 @@
-function Item(props) {
+import PropTypes from 'prop-types'
+
+function Item({nome, breveDescricao}) {
   return (
     <>
-      <li>{props.nome}</li>
+      <li>{nome} - {breveDescricao}</li>
     </>
   )
+}
+
+Item.propTypes = {
+  nome: PropTypes.string.isRequired,
+  breveDescricao: PropTypes.string,
+}
+
+Item.defaultProps = {
+  nome: "Nome desconhecido",
+  breveDescricao: "Nada a comentar",
 }
 
 export default Item;
--- meu-projeto/src/components/List.js
+++ meu-projeto/src/components/List.js
@@ -6,8 +6,9 @@ function List() {
     <>
       <h1>Folclore Maluco</h1>
       <ul className={styles.list}>
-        <Item nome="Chupacabra" />
-        <Item nome="Saci-pererê" />
+        <Item nome="Chupacabra" breveDescricao="Uma criatura que suga o sangue de cabras"/>
+        <Item nome="Saci-pererê" breveDescricao="Ser travesso que usa um gorro vermelho e só tem uma perna"/>
+        <Item />
       </ul>
     </>
   )
